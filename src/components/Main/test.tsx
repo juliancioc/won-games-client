65fee845abdcd7b2678d00525ed7b320aa6e58f3
Juliancio Carvalho <juliancio_alves@hotmail.com>
2022-03-22 23:07:27 -0300

    first commit

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react'
+
+import Main from '.'
+
+describe('<Main />', () => {
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({
+      'background-color': '#06092b'
+    })
+  })
+})
