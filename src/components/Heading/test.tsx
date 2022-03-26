a3cd1b89491d5d92a13f96085c7b0b9006bddbbd
Juliancio Carvalho <juliancio_alves@hotmail.com>
2022-03-26 16:58:35 -0300

    feat: media match

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -4,10 +4,19 @@ import { renderWithTheme } from 'utils/tests/helpers'
 import Heading from '.'
 
 describe('<Heading />', () => {
-  it('should render a white heading', () => {
+  it('should render a white heading by default', () => {
     renderWithTheme(<Heading>Won Games</Heading>)
+    
     expect(screen.getByRole('heading', { name: /Won Games/i })).toHaveStyle({ 
       color: '#FAFAFA' 
+    })
+  })
+
+  it('should render a black heading when is passed', () => {
+    renderWithTheme(<Heading color='black'>Won Games</Heading>)
+
+    expect(screen.getByRole('heading', { name: /Won Games/i })).toHaveStyle({ 
+      color: '#030517' 
     })
   })
 })
