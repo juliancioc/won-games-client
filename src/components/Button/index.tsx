f538c40fa2b88e136eaa204a8429218b53e45bea
Juliancio Carvalho <juliancio_alves@hotmail.com>
2022-03-27 22:07:30 -0300

    feat: button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,23 @@
-import { ReactNode } from 'react'
+import { ReactNode, MouseEvent } from 'react'
+
 import * as S from './styles'
 
 export type ButtonProps = {
   children?: ReactNode
   size?: 'small' | 'medium' | 'large'
   fullWidth?: boolean
+  icon?: ReactNode
+  onClick?: () => (event: MouseEvent<HTMLButtonElement>) => void
 }
 
-const Button = ({ children, size = 'medium', fullWidth = false }: ButtonProps) => (
-  <S.Wrapper size={size} fullWidth={fullWidth}>
+const Button = ({ 
+  children, 
+  size = 'medium', 
+  fullWidth = false, 
+  icon, ...props 
+}: ButtonProps) => (
+  <S.Wrapper size={size} fullWidth={fullWidth} {...props} hasIcon={!!icon}>
+    {!!icon && icon}
     {!!children && <span>{children}</span>}
   </S.Wrapper>
 )
--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,3 +1,4 @@
+import { AddShoppingCart } from '@styled-icons/material-outlined'
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
@@ -5,13 +6,15 @@ import Button from '.'
 
 describe('<Button />', () => {
   it('should render the medium size by default', () => {
-    renderWithTheme(<Button>Buy now</Button>)
+    const { container } = renderWithTheme(<Button>Buy now</Button>)
 
    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
       height: '4rem',
       padding: '0.8rem 3.2rem',
       'font-size': '1.4rem',
    })
+
+   expect(container.firstChild).toMatchSnapshot()
   })
 
   it('should render the small size', () => {
@@ -39,5 +42,12 @@ describe('<Button />', () => {
     expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
       width: '100%'
     })
+  })
+
+  it('should render an icon version', () => {
+    renderWithTheme(<Button icon={<AddShoppingCart data-testid="icon" />}>Buy now</Button>)
+  
+    expect(screen.getByTestId('icon')).toBeInTheDocument()
+    expect(screen.getByText(/buy now/i)).toBeInTheDocument()
   })
 })
